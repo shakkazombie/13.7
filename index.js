4765aa7f3b39c9659da1984c59fec9505a34f036
Lukasz Babicki <lukaszbabicki@gmail.com>
2018-05-01 03:20:03 +0200

    Add next task

---
+++ index.js
@@ -0,0 +1,29 @@
+var fs = require("fs");
+var StatMode = require("stat-mode");
+var colors = require("colors");
+
+fs.stat("./cat.jpg", function(err, stats) {
+    var statMode = new StatMode(stats);
+    console.log(statMode.toString());
+});
+
+fs.readFile("./text.txt", "utf-8", function(err, data) {
+    console.log("zapisz przed".blue);
+    console.log(data);
+    fs.appendFile("./text.txt", "\nLorem ipsum dfakfalfdadalk", function(err) {
+        if (err) throw err;
+        console.log("Saved!".blue);
+        fs.readFile("./text.txt", "utf-8", function(err, data) {
+            console.log("zapisz po".red);
+            console.log(data);
+        });
+    });
+});
+
+fs.readdir("./", "utf-8", function(err, files) {
+    fs.writeFile("./tekst1.txt", files, "utf-8", function(err) {
+        if (err) throw err;
+        console.log("Zapisano");
+        console.log(files);
+    });
+});
